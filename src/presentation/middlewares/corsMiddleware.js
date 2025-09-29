6909d70292b19aeb98493a0f41f2087cc9001eb4
Yorman4312 <yormanalirio4312@gmail.com>
2025-09-29 10:35:40 -0500

    URL Render

--- src/presentation/middlewares/corsMiddleware.js
+++ src/presentation/middlewares/corsMiddleware.js
@@ -144,7 +144,7 @@ import cors from "cors";
  * @returns {Function} Función middleware que procesa peticiones CORS
  */
 const corsMiddleware = cors({
-  origin: ["http://localhost:5173", "http://localhost:4312", "http://127.0.0.1:4312"], 
+  origin: ["https://stockandpedidosapi.onrender.com", "http://localhost:5173", "http://localhost:4312", "http://127.0.0.1:4312"], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 });
