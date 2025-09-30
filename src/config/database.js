6d95a751a429db8fec362cefe54620079030b1f3
Yorman4312 <yormanalirio4312@gmail.com>
2025-09-29 19:35:36 -0500

    Database modifiedv1.0

--- src/config/database.js
+++ src/config/database.js
@@ -37,11 +37,11 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS : 5000,
-      socketTimeoutMS : 45000,
-      family : 4,
-      keepAlive : true,
-      keepAliveInitialDelay : 300000
+      // serverSelectionTimeoutMS : 5000,
+      // socketTimeoutMS : 45000,
+      // family : 4,
+      // keepAlive : true,
+      // keepAliveInitialDelay : 300000
     });
     console.log("✅ Conectado a MongoDB ✅");
   } catch (err) {
